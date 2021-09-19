2b9373a1aec3e200a645ebdb2a8cc7358b3f00cb
David Moftakhar <davidmoftakhar@gmail.com>
2021-09-19 18:40:08 +0100

    Initial hard coded routes.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Basic Blog</title>
+    
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post">First Blog Post</a></h1>
+        
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores eaque et perferendis in, omnis libero? Ducimus dolorem pariatur porro maiores facilis aliquam? Sapiente, placeat. Obcaecati unde nostrum, ea, totam iusto architecto recusandae distinctio optio nisi minus blanditiis repellat magnam consequatur beatae nihil cum praesentium aspernatur. Explicabo possimus cum exercitationem minus!
+        </p>
+        
+        <h1><a href="/post">Second Blog Post</a></h1>
+        
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores eaque et perferendis in, omnis libero? Ducimus dolorem pariatur porro maiores facilis aliquam? Sapiente, placeat. Obcaecati unde nostrum, ea, totam iusto architecto recusandae distinctio optio nisi minus blanditiis repellat magnam consequatur beatae nihil cum praesentium aspernatur. Explicabo possimus cum exercitationem minus!
+        </p>
+        
+        <h1><a href="/post">Third Blog Post</a></h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores eaque et perferendis in, omnis libero? Ducimus dolorem pariatur porro maiores facilis aliquam? Sapiente, placeat. Obcaecati unde nostrum, ea, totam iusto architecto recusandae distinctio optio nisi minus blanditiis repellat magnam consequatur beatae nihil cum praesentium aspernatur. Explicabo possimus cum exercitationem minus!
+        </p>
+
+    </article>
+
+
+    
+</body>
+
+</html>
