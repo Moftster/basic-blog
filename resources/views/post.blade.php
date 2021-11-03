e03fd6db33fb77785ce80a2d6a671c18692292c9
David Moftakhar <davidmoftakhar@gmail.com>
2021-11-03 10:48:25 +0000

    Retrieve post via slug.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
-    
-        <p>{!! $post->body !!}</p>
-        
+
+        <p>{!! $post->content !!}</p>
+
         <a href="/">Go back</a>
     </article>
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
     @foreach ($posts as $post)
         <article>
-            <h1> 
+            <h1>
                 <a href="/posts/{{ $post->slug }}">
                     {{ $post->title }}
                 </a>
             </h1>
-            
+
             <div>
                 <p>{{ $post->excerpt }}</p>
             </div>
