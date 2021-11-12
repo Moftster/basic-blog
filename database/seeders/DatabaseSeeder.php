a1cbccc29bc6fda8b05390f7061530f2e562883a
David Moftakhar <davidmoftakhar@gmail.com>
2021-11-12 11:58:12 +0000

    Basic layout.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Dave Mofty'
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(15)->create([
             'user_id' => $user->id
         ]);
     }
