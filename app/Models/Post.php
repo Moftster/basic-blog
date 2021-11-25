6d0bd6101b5f1fc3878c562e5b71cdf44babd1c0
David Moftakhar <davidmoftakhar@gmail.com>
2021-11-25 20:02:29 +0000

    Include search on the site.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,14 @@ class Post extends Model
 
     protected $with = ['category', 'author'];
 
+    public function scopeFilter($query, array $filters) // Post::newQuery()->filter()
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) => // the fn is just a fancy way of writing function () {}
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('content', 'like', '%' . $search . '%'));
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Models\Category;
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
@@ -18,18 +19,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view ('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all(),
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('/posts/{post:slug}', function(Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function (Category $category){
     return view ('posts', [
