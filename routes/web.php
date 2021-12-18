778bef0a4e86b5929f6d262793f9b80eabe9aede
David Moftakhar <davidmoftakhar@gmail.com>
2021-12-18 09:28:53 +0000

    REgister new user.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 // Route::get('categories/{category:slug}', function (Category $category){
 //     return view ('posts', [
