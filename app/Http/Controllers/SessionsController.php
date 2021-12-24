1ae85d143f307f159dd909d5068945a7be5c6b81
David Moftakhar <davidmoftakhar@gmail.com>
2021-12-24 10:34:02 +0000

    Create session.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,20 @@ namespace App\Http\Controllers;
 
 class SessionsController extends Controller
 {
+    public function create ()
+    {
+        return view('sessions.create');
+    }
+
+    public function store ()
+    {
+        request()->validate([
+            'email' => 'required|exists:users,email'
+        ]);
+
+        return view('sessions.create');
+    }
+
     public function destroy()
     {
         auth()->logout();
