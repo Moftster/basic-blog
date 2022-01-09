6d873df1458562a0f3916f39f8430de8a3826480
David Moftakhar <davidmoftakhar@gmail.com>
2022-01-09 12:41:17 +0000

    Comment form design.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,6 +51,30 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form action="#" method="post">
+                            @csrf
+
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+
+                                <h2 class="ml-3">Add a comment</h2>
+                            </header>
+
+                            <div class="mt-4">
+                                <textarea name="body" class="w-full focus:outline-none focus:ring" id="" rows="5" placeholder="Add your comment here..."></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-4 border-t border-gray-200 pt-6">
+                                <button
+                                    type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-sm py-2 px-10 rounded-2xl hover:bg-blue-600"
+                                >
+                                    Post
+                                </button>
+                            </div>
+                        </form>
+                    </x-panel>
                     @foreach($post->comments as $comment)
                         <x-post-comment
                             :comment="$comment"
