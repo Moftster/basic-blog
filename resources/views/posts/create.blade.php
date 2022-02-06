0c896f518c1f8a0a3890d467889ee7a1b50a727d
David Moftakhar <davidmoftakhar@gmail.com>
2022-02-06 14:42:35 +0000

    Create posts.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,113 @@
 <x-layout>
     <section class="px-6 py-8">
-        This is the Create Page.
+        <x-panel class="max-w-sm mx-auto">
+            <form action="/admin/posts" method="post">
+                @csrf
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="title">
+                        Title
+                    </label>
+
+                    <input
+                        type="text"
+                        class="border border-gray-400 p-2 w-full"
+                        name="title"
+                        id="title"
+                        value="{{ old('title') }}"
+                        required
+                    >
+
+                    @error('title')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="slug">
+                        Slug
+                    </label>
+
+                    <input
+                        type="text"
+                        class="border border-gray-400 p-2 w-full"
+                        name="slug"
+                        id="slug"
+                        value="{{ old('slug') }}"
+                        required
+                    >
+
+                    @error('slug')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="excerpt">
+                        Excerpt
+                    </label>
+
+                    <textarea
+                        type="text"
+                        class="border border-gray-400 p-2 w-full"
+                        name="excerpt"
+                        id="excerpt"
+                        required
+                    >{{ old('excerpt') }}</textarea>
+
+                    @error('excerpt')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="content">
+                        Content
+                    </label>
+
+                    <textarea
+                        type="text"
+                        class="border border-gray-400 p-2 w-full"
+                        name="content"
+                        id="content"
+                        required
+                    >{{ old('content') }}</textarea>
+
+                    @error('content')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="category_id">
+                        Category
+                    </label>
+
+                    <select name="category_id" id="category_id">
+                        @foreach (\App\Models\Category::all() as $category)
+                            <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>{{ ucwords($category->name) }}</option>
+                        @endforeach
+                    </select>
+
+                    @error('category_id')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <x-submit-button>
+                    Publish
+                </x-submit-button>
+            </form>
+        </x-panel>
     </section>
 </x-layout>
