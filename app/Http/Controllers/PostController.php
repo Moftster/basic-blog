0331999ddd2320198f61e33a0cefe2473b1bb964
David Moftakhar <davidmoftakhar@gmail.com>
2022-02-20 18:23:43 +0000

    Admin update and delete posts.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Validation\Rule;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -24,29 +20,5 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
-    }
-
-    public function create()
-    {
-        return view('posts.create');
-    }
-
-    public function store()
-    {
-        $attributes = request()->validate([
-            'title' => 'required',
-            'thumbnail' => 'required|image',
-            'slug' => ['required', Rule::unique('posts', 'slug')],
-            'excerpt' => 'required',
-            'content' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')],
-        ]);
-
-        $attributes['user_id'] = auth()->id();
-        $attributes['thumbnail'] = request()->file('thumbnail')->store('public/thumbnails');
-
-        Post::create($attributes);
-
-        return redirect('/');
     }
 }
